aa6969acd1f1fbf638b498552e9bdca605a05834
Laksh Gupta <laksh.gupta@ext.petroit.com>
2021-12-08 00:13:09 -0800

    initial training attempt

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as o
+import torch.utils.data
 import tqdm
 import torch.utils.data as tud
 from utils import Dataset
 import numpy as np
+import multiprocessing
+import matplotlib.pyplot as plt
+
+
 
 def process_data():
     spectrogram_file = "spectrogram"
@@ -20,6 +25,8 @@ def process_data():
     wavelets_train = []
     spectrogram_test = []
     wavelets_test = []
+
+    print("Started Procesing Data")
 
     spectrogram_train_file = os.path.join(spectrogram_file, "train")
     class_idx = 0
@@ -51,6 +58,8 @@ def process_data():
     train = list(zip(wavelets_train, spectrogram_train))
     test = list(zip(wavelets_test, spectrogram_test))
     # train is a list of tuples, each tuple has two baby tuples, each baby tuple has (image (wavelets/spectrogram), class_idx)
+    # [((), ()),((),()),...]
+    print("Finished Loading data")
     return train, test
 
 
@@ -72,26 +81,54 @@ def train_model(model, train_loader, optimizer, epoch, log_interval):
     return np.mean(losses)
 
 def train_main():
-    train, test = process_data()
-    train = tud.Dataset()
-    test = t.tensor(test)
+    # Play around with these constants, you may find a better setting.
+    # BATCH_SIZE = 256
+    # TEST_BATCH_SIZE = 10
+    # EPOCHS = 20
+    # LEARNING_RATE = 0.01
+    # MOMENTUM = 0.65
+    USE_CUDA = True
+    # SEED = 0
+    # PRINT_INTERVAL = 100
+    # WEIGHT_DECAY = 0.000
 
-    net = MusicGenreNet() # .cuda()
+    # Now the actual training code
+    use_cuda = USE_CUDA and torch.cuda.is_available()
+
+    # torch.manual_seed(SEED)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print('Using device', device)
+
+    print('num cpus:', multiprocessing.cpu_count())
+
+    kwargs = {'num_workers': 6,
+              'pin_memory': False} if use_cuda else {}
+
+
+
+    train, test = process_data()
+    net = MusicGenreNet().to(device)
 
     epochs = 1
     lr = 0.01
     momentum = 0.65
     decay = 0
+    BATCH_SIZE = 2
     optim = o.SGD(net.parameters(), lr=lr, momentum=momentum)
-    train_loader = Dataset(train, batch_size=128)
+    train_load_temp = Dataset(train)
+    train_loader = torch.utils.data.DataLoader(train_load_temp, batch_size=BATCH_SIZE,
+                                               shuffle=True, **kwargs)
     train_losses = []
 
     for epoch in range(epochs):
+        print(f'Starting Epoch : {epoch}')
         train_loss = train_model(net, train_loader, optim, epoch, 1)
         train_losses.append(train_loss)
+
         # collect test loss too
 
-
+    print(f'train losses = {train_losses}')
 
 TEMPERATURE = 0.35
 
@@ -99,7 +136,8 @@ TEMPERATURE = 0.35
 class MusicGenreNet(nn.Module):
     def __init__(self):
         super(MusicGenreNet, self).__init__()
-        self.conv1 = nn.Conv2d(6, 16, (3, 3), stride=(2, 2))
+        #self.conv1 = nn.Conv2d(6, 16, (3, 3), stride=(2, 2))
+        self.conv1 = nn.Conv2d(3, 16, (3, 3), stride=(2, 2))
         self.conv2 = nn.Conv2d(16, 32, (3, 3), stride=(2, 2))
         self.conv3 = nn.Conv2d(32, 64, (3, 3), stride=(2, 2))
         self.linear = nn.Linear(1024, 10)
@@ -119,4 +157,7 @@ class MusicGenreNet(nn.Module):
         loss_val = F.cross_entropy(prediction.view(-1, self.vocab_size), label.view(-1), reduction=reduction)
         return loss_val
 
-# process_data()
+
+if __name__ == '__main__':
+    #freeze_support()
+    train_main()
--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import torch
 import torch.utils.data
+import numpy as np
 
 class Dataset(torch.utils.data.Dataset):
   'Characterizes a dataset for PyTorch'
-  def __init__(self, list_IDs, labels):
+  def __init__(self, train):
         'Initialization'
-        self.labels = labels
-        self.list_IDs = list_IDs
+        self.train = np.asarray(train)
+        self.labels = [x[1][1] for x in self.train]
+        self.list_IDs = np.arange(1, len(train)+1)
 
   def __len__(self):
         'Denotes the total number of samples'
@@ -18,6 +20,9 @@ class Dataset(torch.utils.data.Dataset):
         ID = self.list_IDs[index]
 
         # Load data and get label
-        X = torch.load('data/' + ID + '.pt')
+        print(type(self.train[index][0][0]))
+        #X = np.concatenate(self.train[index][0][0], self.train[index][1][0], axis=2)
+        X = np.concatenate((self.train[index][0][0], self.train[index][1][0]), axis=2)
+        print(X.shape)
         y = self.labels[ID]
         return X, y
