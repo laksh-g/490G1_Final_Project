fddc3facb8b69b9fe98100b4b674e1c4ba5e8ee2
Laksh Gupta <lakshgupta2000@gmail.com>
2021-12-16 15:28:54 -0800

    added result window

--- GUI.py
+++ GUI.py
@@ -9,32 +9,24 @@ def open_window():
     window = Tk()
     # Set window title
     window.title('Music Genre Detector')
-
     # Set window size
     window.geometry("500x500")
-
     # Set window background color
     window.config(background="WHITE")
-
     # Create a File Explorer label
     label_file_explorer = Label(window,
                                 text="Please select the music file to analyze",
                                 width=100, height=4,
                                 fg="blue")
-
     button_explore = Button(window,
                             text="Browse Files",
                             command=browseFiles)
-
     button_exit = Button(window,
                          text="Exit",
                          command=exit)
     label_file_explorer.grid(column=1, row=1)
-
     button_explore.grid(column=1, row=2)
-
     button_exit.grid(column=1, row=3)
-
     return window, label_file_explorer
 
 def browseFiles():
@@ -49,6 +41,27 @@ def browseFiles():
     label_file_explorer.configure(text="File Opened: " + filename)
     close_window(window)
     return filename
+
+def open_results_window(results):
+    res_window = Tk()
+    # Set window title
+    res_window.title('Music Genre Detector')
+    # Set window size
+    res_window.geometry("500x500")
+    # Set window background color
+    res_window.config(background="WHITE")
+    # Create a File Explorer label
+    label_file_explorer = Label(res_window,
+                                text="The results are as follows",
+                                width=100, height=4,
+                                fg="blue")
+    button_exit = Button(res_window,
+                         text="Exit",
+                         command=exit)
+    label_file_explorer.grid(column=1, row=1)
+    button_exit.grid(column=1, row=3)
+    res_window.mainloop()
+    return res_window
 
 def close_window(window):
     window.destroy()
