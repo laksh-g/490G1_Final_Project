b17fd10b5d9221d9033a94032bec976343b4b4e3
Laksh Gupta <laksh.gupta@ext.petroit.com>
2021-12-16 02:14:22 -0800

    clean up

--- front_end.py
+++ front_end.py
@@ -1,2 +1,77 @@
-i = input('specify image path')
-print(i)
+import os
+import matplotlib.pyplot as plt
+import librosa.display
+import sklearn
+from main_test import MusicGenreNet
+import torchvision.io
+import torchvision.transforms as T
+import torch
+import multiprocessing
+
+USE_CUDA = False
+use_cuda = USE_CUDA and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+print('Using device', device)
+print('num cpus:', multiprocessing.cpu_count())
+kwargs = {'num_workers': 6,
+          'pin_memory': False} if use_cuda else {}
+
+class application:
+    def audio_to_wavelets(self, mp3):
+        mp3_title = os.path.basename(mp3)
+        mp3_path = os.path.join(os.getcwd(), mp3_title)
+        x, sr = librosa.load(mp3_path)
+        librosa.display.waveplot(x)
+        if not os.path.exists('temp'):
+            os.mkdir('temp')
+        save_path = os.path.join(os.getcwd(), 'temp', mp3_title + '_wavelet.png')
+        plt.savefig(save_path)
+        plt.close()
+        return save_path
+
+
+    def audio_to_spectrogram(self, mp3):
+        mp3_title = os.path.basename(mp3)
+        mp3_path = os.path.join(os.getcwd(), mp3_title)
+        x, sr = librosa.load(mp3_path)
+        X = librosa.stft(x)
+        Xdb = librosa.amplitude_to_db(abs(X))
+        librosa.display.specshow(Xdb)
+        save_path = os.path.join(os.getcwd(), 'temp', mp3_title + '_spectrogram.png')
+        plt.savefig(save_path)
+        plt.close()
+        return save_path
+
+    def get_inputs(self):
+        print("Please enter audio file path")
+        # audio_file = input()
+        audio_file = "still got the blues.mp3"
+        print(f"Extracting wavelet information from {audio_file}...")
+        spectrogram = app.audio_to_spectrogram(audio_file)
+        print(f"Extracting spectrogram information from {audio_file}...")
+        wavelet =  app.audio_to_wavelets(audio_file)
+        print("Enter model path")
+        # model_path = input()
+        model_path = "C:\\Users\\laksh\\Desktop\\UW\\Deep Learning\\saved_models\\model_1.5"
+        print(f"Loading model...")
+        net = MusicGenreNet()
+        net.load_model(model_path)
+        return audio_file, spectrogram, wavelet, net
+
+    def classify_audio(self, audio_file, wavelet, spectrogram, model):
+        img = torchvision.io.read_image(spectrogram)
+        transform = T.Compose([
+                               T.Resize((288, 432)),
+                            ])
+
+        x = transform(img)
+        x = torch.unsqueeze(x, 0)
+        genre = model.classify_single_input(model, x, device)
+        print(f"Classified {audio_file} as {genre}")
+        return genre
+
+if __name__ == '__main__':
+    app = application()
+    audio_file, spectrogram, wavelet, model = app.get_inputs()
+    genre = app.classify_audio(audio_file, spectrogram, wavelet, model)
+
--- main_test.py
+++ main_test.py
@@ -18,62 +18,12 @@ from utils import Dataset
 import numpy as np
 import multiprocessing
 import matplotlib.pyplot as plt
+import torchvision.transforms as T
 
 IMG_SIZE = 256
 
 labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
 img_size = 256
-def get_data(data_dir):
-    data = []
-    for label in labels:
-        path = os.path.join(data_dir, label)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv2.imread(os.path.join(path, img))[..., ::-1]  # convert BGR to RGB format
-                resized_arr = cv2.resize(img_arr, (img_size, img_size))  # Reshaping images to preferred size
-                resized_arr = resized_arr.reshape((3, 256, 256))
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print(e)
-    return np.array(data)
-
-def tf_process_data():
-    train = get_data('spectrogram/train')
-    val = get_data('spectrogram/test')
-
-    x_train = []
-    y_train = []
-    x_val = []
-    y_val = []
-
-    for feature, label in train:
-        x_train.append(feature)
-        y_train.append(label)
-
-    for feature, label in val:
-        x_val.append(feature)
-        y_val.append(label)
-
-    # Normalize the data
-    x_train = np.array(x_train) / 255
-    x_val = np.array(x_val) / 255
-
-    x_train.reshape(-1, img_size, img_size, 1)
-    y_train = np.array(y_train)
-
-    x_val.reshape(-1, img_size, img_size, 1)
-    y_val = np.array(y_val)
-
-    my_dataset = TensorDataset(torch.tensor(x_train), torch.tensor(y_train))
-    dataloader_train = torch.utils.data.DataLoader(my_dataset)
-
-    my_dataset_test = TensorDataset(torch.tensor(x_val), torch.tensor(y_val))
-    dataloader_test = torch.utils.data.DataLoader(my_dataset_test)
-
-    return dataloader_train, dataloader_test
-
-
 
 def process_data(batch_size):
     spectrogram_file = "spectrogram"
@@ -83,6 +33,11 @@ def process_data(batch_size):
     spectrogram_test = []
     wavelets_test = []
 
+    transform = T.Compose([
+        T.Resize((288, 432)),
+        # T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+
     print("Started Procesing Data")
 
     spectrogram_train_file = os.path.join(spectrogram_file, "train")
@@ -91,27 +46,27 @@ def process_data(batch_size):
         for image in glob.glob(os.path.join(class_name, "*")):
             # print(type(torch.from_numpy(cv2.resize(cv2.imread(image), (IMG_SIZE, IMG_SIZE)))))
             # print(f"data while processing: {torchvision.io.read_image(image).shape}")
-            spectrogram_train.append((torchvision.io.read_image(image), class_idx)) # fix image read
+            spectrogram_train.append((transform(torchvision.io.read_image(image)), class_idx)) # fix image read
         class_idx+=1
     spectrogram_test_file = os.path.join(spectrogram_file, "test")
     class_idx = 0
     for class_name in glob.glob(os.path.join(spectrogram_test_file, "*")):
         for image in glob.glob(os.path.join(class_name, "*")):
-            spectrogram_test.append((torchvision.io.read_image(image), class_idx)) # fix image read
+            spectrogram_test.append((transform(torchvision.io.read_image(image)), class_idx)) # fix image read
         class_idx+=1
 
     wavelets_train_file = os.path.join(wavelets_file, "train")
     class_idx = 0
     for class_name in glob.glob(os.path.join(wavelets_train_file, "*")):
         for image in glob.glob(os.path.join(class_name, "*")):
-            wavelets_train.append((torchvision.io.read_image(image), class_idx))# fix image read
+            wavelets_train.append((transform(torchvision.io.read_image(image)), class_idx))# fix image read
         class_idx+=1
 
     wavelets_test_file = os.path.join(wavelets_file, "test")
     class_idx = 0
     for class_name in glob.glob(os.path.join(wavelets_test_file, "*")):
         for image in glob.glob(os.path.join(class_name, "*")):
-            wavelets_test.append((torchvision.io.read_image(image), class_idx)) # fix image read
+            wavelets_test.append((transform(torchvision.io.read_image(image)), class_idx)) # fix image read
         class_idx+=1
 
     train = spectrogram_train
@@ -124,7 +79,8 @@ def process_data(batch_size):
     my_dataset = TensorDataset(train_x, tr_y)
     dataloader_train = torch.utils.data.DataLoader(my_dataset)
     for (x, y) in dataloader_train:
-        print(f"Sample data from data_loader: {x.shape}, {y}")
+        print(f"Sample data from data_loader: {type(x)}, {y}")
+        print(x.shape)
         break
 
 
@@ -164,13 +120,13 @@ def test_model(model, test_loader, device):
     test_accuracy = 100. * correct / len(test_loader.dataset)
     return test_loss, test_accuracy
 
+
 def train_model(model, train_loader, optimizer, epoch, log_interval, device):
     model.train()
     losses = []
-    # for param in model.parameters():
-    #     print(param.data)
     for batch_idx, (data, label) in enumerate(train_loader):
         data = data.to(device)
+        data = data.float()
         label = torch.tensor(label)
         label = label.to(device)
         optimizer.zero_grad()
@@ -178,18 +134,15 @@ def train_model(model, train_loader, optimizer, epoch, log_interval, device):
         output = output.reshape(10)
         output = output.type(torch.FloatTensor)
         output = torch.reshape(output, (1, 10))
-        # print(output.shape)
-        # print(label.view(-1).shape)
         loss = F.cross_entropy(output, label.view(-1))
         loss.backward()
         optimizer.step()
-        # print(model.conv2.weight.grad)
         a = loss.item()
         losses.append(a)
     return np.mean(losses)
 
 
-def train_main():
+def train_main(version):
     USE_CUDA = False
     use_cuda = USE_CUDA and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
@@ -199,15 +152,14 @@ def train_main():
               'pin_memory': False} if use_cuda else {}
 
     BATCH_SIZE = 64
-    epochs = 50
-    lr = 5e-5
+    epochs = 500
+    lr = 2e-5
     momentum = 0.65
     decay = 0
     train_loader, test_loader = process_data(BATCH_SIZE)
-    # train, test = tf_process_data()
     net = MusicGenreNet().to(device)
     # optimizer = o.SGD(net.parameters(), lr=lr)
-    optimizer = o.Adam(net.parameters(), lr=lr, momentum=momentum)
+    optimizer = o.Adam(net.parameters(), lr=lr)
     train_losses = []
 
     for epoch in range(epochs):
@@ -216,8 +168,11 @@ def train_main():
         print(f"Epoch {epoch} completed: training loss = {train_loss}")
 
     test_loss, test_accuracy = test_model(net, test_loader, device)
+    models_path = os.path.join(os.getcwd(), "saved_models", f"model_{version}")
+    net.save_model(models_path)
     print(f'Final train loss = {train_losses[-1]}')
     print(f'Final test accuracy: {test_accuracy}')
+    return train_losses
 
 
 TEMPERATURE = 0.35
@@ -226,22 +181,13 @@ TEMPERATURE = 0.35
 class MusicGenreNet(nn.Module):
     def __init__(self):
         super(MusicGenreNet, self).__init__()
-        # self.conv1 = nn.Conv2d(3, 8, (3, 3), (2, 2))
-        # self.maxpool1 = nn.MaxPool2d(3, 2)
-        # self.conv2 = nn.Conv2d(8, 12, (4, 4), (2, 2))
-        # self.maxpool2 = nn.MaxPool2d(3, 2)
-        # self.conv3 = nn.Conv2d(12, 16, (5, 5), (2, 2))
-        # self.maxpool3 = nn.MaxPool2d(3, 2)
-        # self.linear1 = nn.Linear(160, 64)
-        # self.linear2 = nn.Linear(64, 10)
-
         self.conv1 = nn.Conv2d(4, 8, (3, 3), (2, 2))
         self.maxpool1 = nn.MaxPool2d(3, 2)
-        self.conv2 = nn.Conv2d(8, 12, (4, 4), (2, 2))
+        self.conv2 = nn.Conv2d(8, 16, (4, 4), (2, 2))
         self.maxpool2 = nn.MaxPool2d(3, 2)
-        self.conv3 = nn.Conv2d(12, 16, (5, 5), (2, 2))
+        self.conv3 = nn.Conv2d(16, 24, (5, 5), (2, 2))
         self.maxpool3 = nn.MaxPool2d(3, 2)
-        self.linear1 = nn.Linear(160, 64)
+        self.linear1 = nn.Linear(240, 64)
         self.linear2 = nn.Linear(64, 10)
 
     def forward(self, x):
@@ -262,12 +208,6 @@ class MusicGenreNet(nn.Module):
         return x
 
     def loss(self, prediction, label, reduction='mean'):
-        # print(f"prediction shape: {prediction.shape}")
-        # print(f"label shape: {label.type(torch.LongTensor).shape}")
-        # print(f"prediction: {prediction}")
-        # print(f"label: {label.type(torch.LongTensor)}")
-        # print(f"type1 {type(prediction[0])}")
-        # print(f"type2 {type(label[0])}")
         return F.mse_loss(prediction, label.type(torch.FloatTensor))
 
     def save_model(self, file_path, num_to_keep=1):
@@ -276,5 +216,23 @@ class MusicGenreNet(nn.Module):
     def load_model(self, file_path):
         pt_util.restore(self, file_path)
 
+    def classify_single_input(self, model, data, device):
+        model.eval()
+        with torch.no_grad():
+            data = data.to(device)
+            output = model(data.to(torch.float32))
+            output = output.reshape(10)
+            output = torch.reshape(output, (1, 10))
+            pred = output.max(1)[1]
+        return pred
+
 if __name__ == '__main__':
-    train_main()
+    version = "1.7"
+    train_losses = train_main(version)
+    plt.xlabel = "Epochs"
+    plt.ylabel = "Cross Entropy Loss"
+    plot_path = os.path.join(os.getcwd(), "plots")
+    if not os.path.exists(plot_path):
+        os.mkdir(plot_path)
+    plot_path = os.path.join(plot_path, f"training_loss_{version}.PNG")
+    plt.savefig(plot_path)
